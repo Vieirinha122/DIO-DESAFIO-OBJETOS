436b5227af877f1b9d6bb719bf0c1ea7b30b4d71
Vieirinha122 <arthurv720@gmail.com>
2024-05-14 10:27:44 -0300

    Código finalizado, testado e aprovado.

---
+++ index.js
@@ -0,0 +1,31 @@
+class Hero {
+    constructor(nome, idade, tipo) {
+      this.nome = nome;
+      this.idade = idade;
+      this.tipo = tipo;
+    }
+  
+    atacar() {
+      let ataque;
+      switch (this.tipo) {
+        case 'mago':
+          ataque = 'usou magia';
+          break;
+        case 'guerreiro':
+          ataque = 'usou espada';
+          break;
+        case 'monge':
+          ataque = 'usou artes marciais';
+          break;
+        case 'ninja':
+          ataque = 'usou shuriken';
+          break;
+        default:
+          ataque = 'não se sabe como atacou';
+      }
+      console.log(`O ${this.tipo} ${this.nome} atacou usando ${ataque}`);
+    }
+  }
+  
+  const heroi = new Hero('Vieira', 20, 'monge');
+  heroi.atacar();
